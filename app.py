52ebef1bb0f43cf0fab72e8f9072a19d4b70d048
Deimian Vásquez <deimianvasquez@gmail.com>
2025-05-17 01:34:02 +0000

    add tipos

---
+++ app.py
@@ -0,0 +1,65 @@
+# print("hola")
+
+# Variables --> tipo de datos 
+# num1 = 10 # entero
+# num2 = "15" # string
+# num3 = 10.5
+# num4 = {
+#     "name":"Deimian"
+# }
+# num5 = ["hola"]
+# num_two = "Hola ¿qué tal?" 
+# print(num1 + int(num2))
+
+# Tipo de datos --> Number (integer or float)
+# age = 18
+# age_sum = input("ingresa una edad: ")
+# print(age + int(age_sum))
+
+
+# None
+# def num2():
+#     return
+# print(num2())
+
+# Array (lista)
+names_arr = ["Deimian", "Juana", "María"]
+print(type(names_arr))
+
+# Diccionarios
+# human = {
+#     "name":"Deimian Two",
+#     "pets": ["Cat", "Dogs"]
+# }
+
+# print(human["name"])
+
+# # Objetos 
+# class Human():
+#     def __init__(self, name):
+#         self.name = name
+
+#     def result(self):
+#         return self.name
+
+# print(Human("Deimian").result())
+
+# Tuplas
+
+# names = ("Deimian", "Cristian")
+# names[0] = "Otro"
+# print(names)
+
+# names_arr.append("Juanita")
+# print(names_arr)
+
+
+# Set
+# frutas = {"Mango", "Pera", "Manzana"}
+# otra_fruta = {"Mango", "Cambur", "Mandarina"}
+# print(frutas.intersection(otra_fruta))
+
+# string
+
+# saludo = '''HOla ¿qué tal'''
+# print(saludo)
